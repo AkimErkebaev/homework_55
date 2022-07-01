6f6d90a4850ec3d6abd543778670bb3d284fe91c
akim <akimmmm@bk.ru>
2022-07-01 23:45:32 +0600

    Создал базовую страницу html

--- Todolist/webapp/urls.py
+++ Todolist/webapp/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from webapp.views import index_view, create_task
+from webapp.views import index_view, create_task, task_view
 urlpatterns = [
     path('', index_view, name="index"),
     path('tasks/add/', create_task, name="create_task"),
